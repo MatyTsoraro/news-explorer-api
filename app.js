3c028adedb1627ea124d9cfc1d6b86ddf9a2a5a4
MatyTsoraro <maty.tsoraro@gmail.com>
2023-07-28 00:20:45 +0300

    add get /

--- app.js
+++ app.js
@@ -55,6 +55,11 @@ app.use(cors());
 app.use('/users', userRoutes);
 app.use('/articles', articleRoutes);
 
+// Respond to requests to the root the application
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to my API!' });
+});
+
 // Handler for non-existent routes
 app.use((req, res, next) => {
   res.status(404).json({ error: 'Route not found' });
