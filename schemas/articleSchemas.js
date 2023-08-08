ad312b566d878d33f3043c8d4dadf486d9553d0b
MatyTsoraro <maty.tsoraro@gmail.com>
2023-08-08 21:29:43 +0300

    fdsf

--- schemas/articleSchemas.js
+++ schemas/articleSchemas.js
@@ -1,15 +1,17 @@
 const Joi = require('joi');
 
+const urlRegex = /^(https?:\/\/)?([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}(:[0-9]{1,5})?(\/.*)?$/i;
+
 exports.createArticleSchema = Joi.object({
   keyword: Joi.string().required(),
   title: Joi.string().required(),
   text: Joi.string().required(),
   date: Joi.string().required(),
   source: Joi.string().required(),
-  link: Joi.string().uri().required(), // validates URI format
-  image: Joi.string().uri().required(), // validates URI format
+  link: Joi.string().pattern(urlRegex).required(),
+  image: Joi.string().uri().required(),
 });
 
 exports.deleteArticleSchema = Joi.object({
-  articleId: Joi.string().pattern(/^[0-9a-fA-F]{24}$/).required(), // validates MongoDB ObjectId
+  articleId: Joi.string().pattern(/^[0-9a-fA-F]{24}$/).required(),
 });
